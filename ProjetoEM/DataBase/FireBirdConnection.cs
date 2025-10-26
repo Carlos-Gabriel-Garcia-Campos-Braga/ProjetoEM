1759aaa5c5fd4beaf0fff7f61e7e3d47870c5e04
Carlos-Gabriel-Garcia-Campos-Braga <carlosbragadev@gmail.com>
2025-10-26 10:46:22 -0300

    adicionado o banco de dados Firebird

---
+++ ProjetoEM/DataBase/FireBirdConnection.cs
@@ -0,0 +1,16 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+
+namespace ProjetoEM.DataBase
+{
+    public class FireBirdConnection(string connectionString)
+    {
+        private readonly string _connectionString = connectionString;
+
+        public FbConnection CreateConnection()
+        {
+            var connection = new FbConnection(_connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}

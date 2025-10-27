08e9c1a357409b8d339890343d73230ec69a4611
Carlos-Gabriel-Garcia-Campos-Braga <carlosbragadev@gmail.com>
2025-10-27 19:56:08 -0300

    Colocado a validacao do CPF na sua propria classe

--- ProjetoEM/ValueObjects/CPF.cs
+++ ProjetoEM/ValueObjects/CPF.cs
@@ -2,7 +2,7 @@
 {
     public class CPF
     {
-        public string Value { get; set; }
+        public string Value { get; private set; }
 
         public CPF()
         {
@@ -14,9 +14,73 @@
             Value = value;
         }
 
-        public static implicit operator string(CPF cpf) => cpf.Value;
-        public static explicit operator CPF(string value) => new CPF(value);
+        public bool EhValido()
+        {
+            if (string.IsNullOrWhiteSpace(Value))
+            {
+                return false;
+            }
 
-        public override string ToString() => Value;
+            string? cpfLimpo = RemoverFormatacao(Value);
+
+            if (cpfLimpo.Length != 11)
+            {
+                return false;
+            }
+
+            if (cpfLimpo.All(c => c == cpfLimpo[0]))
+            {
+                return false;
+            }
+
+            return ValidarDigitosVerificadores(cpfLimpo);
+        }
+
+        public static string RemoverFormatacao(string cpf)
+        {
+            if (string.IsNullOrWhiteSpace(cpf))
+            {
+                return string.Empty;
+            }
+                
+            return cpf.Replace(".", "").Replace("-", "").Trim();
+        }
+
+        private static bool ValidarDigitosVerificadores(string cpf)
+        {
+            int soma = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                soma += int.Parse(cpf[i].ToString()) * (10 - i);
+            }
+
+            int digito1 = (soma % 11 < 2) ? 0 : 11 - (soma % 11);
+
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                soma += int.Parse(cpf[i].ToString()) * (11 - i);
+            }
+
+            int digito2 = (soma % 11 < 2) ? 0 : 11 - (soma % 11);
+
+            return digito1 == int.Parse(cpf[9].ToString()) && 
+                   digito2 == int.Parse(cpf[10].ToString());
+        }
+
+        public static implicit operator string(CPF cpf)
+        {
+            return cpf.Value;
+        }
+        
+        public static explicit operator CPF(string value)
+        {
+            return new(value);
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
     }
 }

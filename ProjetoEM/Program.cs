3a95728f31d4af7e49aba2e5c209c12eee426ec3
Carlos-Gabriel-Garcia-Campos-Braga <carlosbragadev@gmail.com>
2025-10-29 17:36:33 -0300

    configuracoes de bd e projeto

--- ProjetoEM/Program.cs
+++ ProjetoEM/Program.cs
@@ -1,20 +1,39 @@
 using ProjetoEM.DataBase;
 using ProjetoEM.Interfaces;
 using ProjetoEM.Repositories;
+using ProjetoEM.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var supportedCultures = new[] { new CultureInfo("pt-BR") };
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    options.DefaultRequestCulture = new RequestCulture("pt-BR");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+});
+
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddDataAnnotationsLocalization();
 
 
 string connectionString = builder.Configuration.GetConnectionString("FirebirdConnection");
 builder.Services.AddSingleton(new FireBirdConnection(connectionString));
 
+// Repositories
 builder.Services.AddScoped<IAlunoRepository, AlunoRepository>();
 builder.Services.AddScoped<ICidadeRepository, CidadeRepository>();
 
+// Services
+builder.Services.AddScoped<RelatorioService>();
+
 var app = builder.Build();
+
+// Usar localização
+app.UseRequestLocalization();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -33,6 +52,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Aluno}/{action=Index}/{id?}");
 
 app.Run();

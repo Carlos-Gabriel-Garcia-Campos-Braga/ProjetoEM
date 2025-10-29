e8d05a11d0219aacbc10dd10863898f4d51d78b0
Carlos-Gabriel-Garcia-Campos-Braga <carlosbragadev@gmail.com>
2025-10-29 17:32:31 -0300

    adicionado o relatorio de pdf

---
+++ ProjetoEM/Services/RelatorioService.cs
@@ -0,0 +1,102 @@
+using ProjetoEM.Models;
+
+namespace ProjetoEM.Services
+{
+    public class RelatorioService
+    {
+        public byte[] GerarRelatorioPDFAlunos(IEnumerable<Aluno> alunos)
+        {
+            using var stream = new MemoryStream();
+            var doc = new iTextSharp.text.Document(iTextSharp.text.PageSize.A4, 36, 36, 36, 36);
+            var writer = iTextSharp.text.pdf.PdfWriter.GetInstance(doc, stream);
+            doc.Open();
+
+            var fonteTitulo = new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 16, iTextSharp.text.Font.BOLD);
+            var fonteCabecalho = new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 10, iTextSharp.text.Font.BOLD);
+            var fonteNormal = new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 10, iTextSharp.text.Font.NORMAL);
+            var fonteRodape = new iTextSharp.text.Font(iTextSharp.text.Font.HELVETICA, 8, iTextSharp.text.Font.NORMAL);
+
+            var paragrafoTitulo = new iTextSharp.text.Paragraph("RELATÓRIO DE ALUNOS", fonteTitulo);
+            paragrafoTitulo.Alignment = iTextSharp.text.Element.ALIGN_CENTER;
+            doc.Add(paragrafoTitulo);
+            doc.Add(new iTextSharp.text.Paragraph(" "));
+
+            var paragrafoData = new iTextSharp.text.Paragraph($"Data: {DateTime.Now:dd/MM/yyyy HH:mm}", fonteNormal);
+            paragrafoData.Alignment = iTextSharp.text.Element.ALIGN_RIGHT;
+            doc.Add(paragrafoData);
+            doc.Add(new iTextSharp.text.Paragraph($"Total de Alunos: {alunos.Count()}", fonteNormal));
+            doc.Add(new iTextSharp.text.Paragraph(" "));
+
+            if (alunos.Any())
+            {
+                var tabela = new iTextSharp.text.pdf.PdfPTable(5) { WidthPercentage = 100 };
+                tabela.SetWidths(new float[] { 15, 30, 20, 15, 20 });
+
+                void AdicionarCelula(string texto, iTextSharp.text.Font fonte, bool ehCabecalho = false)
+                {
+                    var cell = new iTextSharp.text.pdf.PdfPCell(new iTextSharp.text.Phrase(texto, fonte));
+                    if (ehCabecalho)
+                    {
+                        cell.BackgroundColor = new iTextSharp.text.BaseColor(238, 238, 238);
+                    }
+                    cell.Padding = 5f;
+                    cell.HorizontalAlignment = iTextSharp.text.Element.ALIGN_CENTER;
+                    tabela.AddCell(cell);
+                }
+
+                void AdicionarCelulaTexto(string texto, iTextSharp.text.Font fonte, int alinhamento = iTextSharp.text.Element.ALIGN_LEFT)
+                {
+                    var cell = new iTextSharp.text.pdf.PdfPCell(new iTextSharp.text.Phrase(texto, fonte));
+                    cell.Padding = 5f;
+                    cell.HorizontalAlignment = alinhamento;
+                    tabela.AddCell(cell);
+                }
+
+                var fonteHeader = new iTextSharp.text.Font(
+                    iTextSharp.text.Font.HELVETICA,
+                    10,
+                    iTextSharp.text.Font.BOLD,
+                    new iTextSharp.text.BaseColor(0, 0, 0));
+
+                AdicionarCelula("Matrícula", fonteHeader, true);
+                AdicionarCelula("Nome", fonteHeader, true);
+                AdicionarCelula("CPF", fonteHeader, true);
+                AdicionarCelula("Sexo", fonteHeader, true);
+                AdicionarCelula("Cidade", fonteHeader, true);
+
+                foreach (var aluno in alunos.OrderBy(a => a.Nome))
+                {
+                    AdicionarCelulaTexto(aluno.Matricula.ToString(), fonteNormal, iTextSharp.text.Element.ALIGN_CENTER);
+                    AdicionarCelulaTexto(aluno.Nome, fonteNormal, iTextSharp.text.Element.ALIGN_LEFT);
+                    AdicionarCelulaTexto(aluno.Cpf?.Value ?? "-", fonteNormal, iTextSharp.text.Element.ALIGN_CENTER);
+                    AdicionarCelulaTexto(aluno.Sexo.ToString(), fonteNormal, iTextSharp.text.Element.ALIGN_CENTER);
+
+                    var cidadeTexto = aluno.Cidade != null
+                        ? $"{aluno.Cidade.NomeDaCidade} - {aluno.Cidade.UF}"
+                        : "-";
+                    AdicionarCelulaTexto(cidadeTexto, fonteNormal, iTextSharp.text.Element.ALIGN_LEFT);
+                }
+
+                doc.Add(tabela);
+            }
+            else
+            {
+                var paragrafoSemDados = new iTextSharp.text.Paragraph("Nenhum aluno cadastrado no sistema.", fonteNormal);
+                paragrafoSemDados.Alignment = iTextSharp.text.Element.ALIGN_CENTER;
+                doc.Add(paragrafoSemDados);
+            }
+
+            doc.Add(new iTextSharp.text.Paragraph(" "));
+            var paragrafoRodape = new iTextSharp.text.Paragraph("Sistema de Gerenciamento Escolar - ProjetoEM", fonteRodape);
+            paragrafoRodape.Alignment = iTextSharp.text.Element.ALIGN_CENTER;
+            doc.Add(paragrafoRodape);
+
+            doc.Close();
+            writer.Close();
+
+            return stream.ToArray();
+        }
+    }
+}
+
+
